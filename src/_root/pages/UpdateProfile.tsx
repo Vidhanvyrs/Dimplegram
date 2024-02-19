f2f38e45113dfa3d4c22d54a4c0970c98d697dfa
Vidhanvyrs <vidhanvyrs@gmail.com>
2024-02-19 16:41:53 +0530

    error resolved

--- src/_root/pages/UpdateProfile.tsx
+++ src/_root/pages/UpdateProfile.tsx
@@ -43,7 +43,7 @@ const UpdateProfile = () => {
 
   // Queries
   const { data: currentUser } = useGetUserById(id || "");
-  const { mutateAsync: updateUser, isLoading: isLoadingUpdate } =
+  const { mutateAsync: updateUser, isPending: isLoadingUpdate } =
     useUpdateUser();
 
   if (!currentUser)
--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -162,15 +162,16 @@ export const useDeletePost = () => {
 export const useGetPosts = () => {
   return useInfiniteQuery({
     queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
-    queryFn: getInfinitePosts as any,
-    getNextPageParam: (lastPage: any) => {
+    queryFn: getInfinitePosts,
+    getNextPageParam: (lastPage) => {
       // If there's no data, there are no more pages.
       if (lastPage && lastPage.documents.length === 0) {
         return null;
       }
-
       // Use the $id of the last document as the cursor.
-      const lastId = lastPage.documents[lastPage.documents.length - 1].$id;
+
+      const lastId = lastPage?.documents[lastPage?.documents.length - 1].$id;
+
       return lastId;
     },
   });
