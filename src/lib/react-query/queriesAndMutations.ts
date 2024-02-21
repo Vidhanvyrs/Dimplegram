26ffb602aea59327d90e3c62e61d00853dfebf3f
Vidhanvyrs <vidhanvyrs@gmail.com>
2024-02-21 16:49:12 +0530

    error resolved phase 3

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -52,6 +52,13 @@ export const useCreatePost = () => {
     },
   });
 };
+export const useSearchPosts = (searchTerm: string) => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.SEARCH_POSTS, searchTerm],
+    queryFn: () => searchPosts(searchTerm),
+    enabled: !!searchTerm,
+  });
+};
 export const useGetRecentPosts = () => {
   return useQuery({
     queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
@@ -162,25 +169,15 @@ export const useDeletePost = () => {
 export const useGetPosts = () => {
   return useInfiniteQuery({
     queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
-    queryFn: getInfinitePosts,
-    getNextPageParam: (lastPage) => {
-      // If there's no data, there are no more pages.
+    queryFn: getInfinitePosts as any,
+    getNextPageParam: (lastPage: any) => {
       if (lastPage && lastPage.documents.length === 0) {
         return null;
       }
-      // Use the $id of the last document as the cursor.
-
-      const lastId = lastPage?.documents[lastPage?.documents.length - 1].$id;
-
+      const lastId = lastPage.documents[lastPage.documents.length - 1].$id;
       return lastId;
     },
-  });
-};
-export const useSearchPosts = (searchTerm: string) => {
-  return useQuery({
-    queryKey: [QUERY_KEYS.SEARCH_POSTS, searchTerm],
-    queryFn: () => searchPosts(searchTerm),
-    enabled: !!searchTerm,
+    initialPageParam: null,
   });
 };
 // export const useGetUserPosts = (userId?: string) => {
